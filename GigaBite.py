89e73c122158c2c92fb5473145298d7a1b3a66c6
Dalwy <DalwyXD12@gmail.com>
2019-09-18 10:52:54 -0500

    Updated GB.py

--- GigaBite.py
+++ GigaBite.py
@@ -1,2 +1,15 @@
 import discord
+from discord.ext import commands
+import json
 
+
+bot = commands.Bot(command_prefix="!")
+def load_creds():
+    with open('credentials.json') as f:
+        return json.load(f)
+
+creds = load_creds()
+token = creds['token']
+
+
+bot.run(token)
