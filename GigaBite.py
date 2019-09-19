27c477f5abfc6b78441f1e01acbaa2a26c6bd4b7
Dalwy <DalwyXD12@gmail.com>
2019-09-19 18:13:23 -0500

    Updated

--- GigaBite.py
+++ GigaBite.py
@@ -3,13 +3,27 @@ from discord.ext import commands
 import json
 
 
-bot = commands.Bot(command_prefix="!")
 def load_creds():
     with open('credentials.json') as f:
         return json.load(f)
 
+
+bot = commands.Bot(command_prefix="!")
+
 creds = load_creds()
 token = creds['token']
+id = bot.get_guild(623651335316963339)
+
+@bot.event
+async def on_ready():
+    print('Welcome to Gigabite: ' + str(bot.user.name) + '\n')
+    print('Id: ' + str(bot.user.id))
+    print('Guilds: ', str(len(bot.guilds)))
+    print('Users: ', str(len(set(bot.get_all_members()))))
+
+@bot.command(pas_context=True)
+async def ping(ctx):
+    await ctx.send("Pong")
 
 
 bot.run(token)
