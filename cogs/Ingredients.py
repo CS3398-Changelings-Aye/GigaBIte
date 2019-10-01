e766e6757a6e1f0488067a87cb63b24b52e63c71
Wilson Benitez <wilsonabenitez98@gmail.com>
2019-10-01 18:13:56 -0500

    Update for Ingredients

--- cogs/Ingredients.py
+++ cogs/Ingredients.py
@@ -11,13 +11,15 @@ class Ingredients(commands.Cog):
     @commands.command()
     async def Cook(self, ctx, userInput):
 
+        # if userInput == "Me" or userInput == "me":
+        #     hi = "Okay bby cakes"
+        #     await ctx.send(hi)
+
         with open(r"IGDBOut.txt", "r") as f:
-            i = 1
             for line in f:
-                if userInput in line:
-                    # await ctx.send(re.sub("\s\s+", " ",str(i) + ") " + line.lower().title().strip()))
-                    print(re.sub("\s\s+", " ",str(i) + ") " + line.lower().title().strip()))
-                    i = i + 1;
+                if (userInput.upper() or userInput.lower()) in line:
+                    await ctx.send(re.sub("\s\s+", " ", "* " + line.lower().title().strip()))
+                    # print(re.sub("\s\s+", " ", "* " + line.lower().title().strip()))
 
 def setup(bot):
     bot.add_cog(Ingredients(bot))
