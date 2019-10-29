aaf98b82372feeae6b9b60ed72643f6d3af180ed
Sebastian Santana <37159083+crossflag@users.noreply.github.com>
2019-10-29 13:05:57 -0500

    Baseline Implementation
    Found an implementation using the beautiful soup parser. The other ways were a bit to complex using Google API keys and permissions could be done just would be an entire massive function in order to parse back links. Will keep working on it

---
+++ Youtube.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+import sys
+import re
+from bs4 import BeautifulSoup
+import csv
+from itertools import zip_longest
+import aiohttp
+import requests
+
+class Youtube(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def Youtube(self, ctx, *, userinput):
+        print(userinput)
+        searchlist = []
+        async with aiohttp.ClientSession() as session:
+            url = 'https://www.youtube.com/search/?'
+            line = " "
+            userinput = {'q': line + userinput}
+            link = session.get(url, params=userinput)
+            vid = await link
+            finder = re.findall(r'(https?://[^)\s]+)', str(vid))
+            vidlist.append(search)
+            for x in range(len(vidlist)):
+                finv = ''.join(finder)
+            print(finv)
+            data = requests.get(finv)
+            em = discord.Embed(title="Search Results", description="I found what you're looking for!")
+            em.colour = 0xFFFA
+            em.add_field(name="Link to site", value=str(search))
+            await ctx.send(embed=em)
+
+            soup = BeautifulSoup(data.text, 'html.parser')
+            print(soup)
+
+def setup(bot):
+    bot.add_cog(Youtube(bot))
