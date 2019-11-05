7a59627723185253bbdb04b2b03dd8a70ab11647
wilson21x <43559667+wilson21x@users.noreply.github.com>
2019-11-05 15:36:46 -0600

    Update Ingredients.py
    Added the ability to pull a photo from google of what the client is searching for

--- cogs/Ingredients.py
+++ cogs/Ingredients.py
@@ -2,6 +2,11 @@ import discord
 from discord.ext import commands
 import sys
 import re
+from bs4 import BeautifulSoup
+import csv
+from itertools import zip_longest
+import aiohttp
+import requests
 
 class Ingredients(commands.Cog):
 
@@ -16,6 +21,38 @@ class Ingredients(commands.Cog):
                 if (userInput.upper() or userInput.lower()) in line:
                     await ctx.send(re.sub("\s\s+", " ", "* " + line.lower().title().strip()))
                     # print(re.sub("\s\s+", " ", "* " + line.lower().title().strip()))
+                    
+@commands.command()
+    async def Search(self, ctx, *, userinput):
+        print(userinput)
+        searchlist = []
+        async with aiohttp.ClientSession() as session:
+            url = 'https://www.google.com/search?'
+            line = "What can I make with "
+            userinput = {'q': line + userinput}
+            link = session.get(url, params=userinput)
+            x = await link
+
+            # var = ([''.join(n.split()) for n in str(x)])
+            search = re.findall(r'(https?://[^)\s]+)', str(x))
+            searchlist.append(search)
+            for x in range(len(searchlist)):
+                y = ''.join(search)
+            print(y)
+            data = requests.get(y)
+            em = discord.Embed(title="Search Results", description="Here is your results")
+            em.colour = 0xFFFA
+            em.add_field(name="Link to site", value=str(search))
+            await ctx.send(embed=em)
+
+            soup = BeautifulSoup(data.text, 'html.parser')
+            recipelist = []
+            Recipe = soup.find_all('div')
+            p = (str(Recipe).split())
+            p.sort()
+            print(p)
+            print('\n' + str(Recipe))
+            recipelist.append(soup)
 
 def setup(bot):
     bot.add_cog(Ingredients(bot))
