03e4141b9d4062ab522a5769d6a779cc618679b7
wilson21x <43559667+wilson21x@users.noreply.github.com>
2019-12-03 13:58:38 -0600

    Update Ingredients.py

--- cogs/Ingredients.py
+++ cogs/Ingredients.py
@@ -22,6 +22,63 @@ class Ingredients(commands.Cog):
                     await ctx.send(re.sub("\s\s+", " ", "* " + line.lower().title().strip()))
                     # print(re.sub("\s\s+", " ", "* " + line.lower().title().strip()))
                     
+var cheerio = require("cheerio"); 
+var request = require("request"); 
+ 
+var discord = require("discord.js");
+var client = new discord.Client();
+
+client.login("private token");
+ 
+client.on("ready", function() {
+    console.log("logged in");
+});
+ 
+client.on("message", function(message) {
+ 
+    var parts = message.content.split(" ");
+    
+    if (parts[0] === "!Search") { // Check if first part of message is image command
+ 
+        image(message, parts); // Pass requester message to image function
+ 
+    }
+ 
+});
+ 
+function image(message, parts) {
+ 
+    var search = parts.slice(1).join(" "); 
+ 
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + search,
+        method: "GET",
+        headers: {
+            "Accept": "text/html",
+            "User-Agent": "Chrome"
+        }
+    };
+    request(options, function(error, response, responseBody) {
+        if (error) {
+            // handle error
+            return;
+        }
+  
+        $ = cheerio.load(responseBody); 
+ 
+        var links = $(".image a.link");
+ 
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+        console.log(urls);
+        if (!urls.length) {
+            return;
+        }
+ 
+        message.channel.send( urls[0] );
+    });
+ 
+}
+/*                    
 @commands.command()
     async def Search(self, ctx, *, userinput):
         print(userinput)
@@ -53,6 +110,7 @@ class Ingredients(commands.Cog):
             print(p)
             print('\n' + str(Recipe))
             recipelist.append(soup)
+*/
 
 def setup(bot):
     bot.add_cog(Ingredients(bot))
